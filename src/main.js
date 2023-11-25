84bacdee186664ad02738495731f830b2d9b7abd
Costbound <popovskiy96@gmail.com>
2023-11-25 05:43:37 -0300

    add tel copy function

--- src/main.js
+++ src/main.js
@@ -14,7 +14,31 @@ menuCloseBtn.onclick = function() {
 // Close menu when activate anchor link
 const anchors = document.querySelectorAll('a[href*="#"]')
 for (let anchor of anchors) {
-    anchor.addEventListener('click', (e) => {
+    anchor.onclick = function() {
         menu.classList.remove("is-open");
-  })
+  }
+}
+
+// Copy phone number when you click on it 
+const delay = async (ms) => { await new Promise(resolve => setTimeout(resolve, ms)) };
+const telNumber = document.querySelector(".tel-link");
+const telSpan = document.querySelector(".tel-link-span");
+telNumber.onclick = async function () {
+    const copyInput = document.createElement("input");
+    copyInput.setAttribute("type", "text");
+    copyInput.setAttribute("value", "+38 (068) 443-94-26");
+    copyInput.setAttribute("class", "temporary-input");
+    menu.append(copyInput);
+    document.querySelector(".temporary-input").select();
+    document.execCommand("copy");
+    document.querySelector(".temporary-input").remove();
+    telSpan.style.display = "block";
+    await delay(20);
+    telSpan.style.transform = "translate(-50%, 150%)";
+    telSpan.style.color = "transparent";
+    await delay(1000);
+    telSpan.style.display = "";
+    telSpan.style.transform = "";
+    telSpan.style.color = "";
+
 }
